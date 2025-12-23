14043bdb5d078ce0a73c67b5c8c62d8e97e51ae2
Hunter Melrose <huntermelrose831@gmail.com>
2025-12-22 16:01:58 -0800

    fix: build errors

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "src/setupTests.js",
+
     include: ["src/**/*.{test,spec}.{js,jsx,ts,tsx}"],
   },
 });
