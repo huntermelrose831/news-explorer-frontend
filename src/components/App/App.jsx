402fb554619094878544d3363376b2a70e320911
Hunter Melrose <huntermelrose831@gmail.com>
2025-12-22 17:13:26 -0800

    fix deploy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "../Main/Main";
 import SavedNews from "../SavedNews/SavedNews";
 import { AuthProvider } from "../../contexts/AuthContext";
 
 function App() {
-  const basename = import.meta.env.BASE_URL || "/";
   return (
     <AuthProvider>
-      <BrowserRouter basename={basename}>
-        <div className="page">
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/saved-news" element={<SavedNews />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div className="page">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/saved-news" element={<SavedNews />} />
+        </Routes>
+      </div>
     </AuthProvider>
   );
 }
